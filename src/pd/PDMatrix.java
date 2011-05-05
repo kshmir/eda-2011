a95a25358593aba5399fa42f794e36e104517bb7
unknown <Eze@.(none)>
2011-05-05 16:31:09 -0300

    commiteando por eze

--- src/pd/PDMatrix.java
+++ src/pd/PDMatrix.java
@@ -2,4 +2,21 @@ package pd;
 
 public class PDMatrix {
 
+	public void siblings (int x, int y){
+		
+	}
+
+	public void get (int x, int y){
+		
+	}
+
+	public void remove (int x, int y){
+		
+	}
+	
+	public void add (int x, int y){
+		
+	}
+
+
 }
