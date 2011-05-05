0ed2af170b6c61c2d85d81e4058749234fad0ed0
Eze <moroco.ortiz@gmail.com>
2011-05-05 18:12:51 -0300

    Eze

---
+++ src/pd/PDMatrixTest.java
@@ -0,0 +1,12 @@
+package pd;
+
+public class PDMatrixTest {
+
+	public static void main(String[] args) {
+		PDMatrix matrix = new PDMatrix(3,4);
+		PDMatrix matrix2 = new PDMatrix(-3,-4);
+		
+
+	}
+
+}
