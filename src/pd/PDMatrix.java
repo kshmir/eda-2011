5bc22734327e18b2a0e90d0b30f6e2014b08eb50
unknown <Eze@.(none)>
2011-05-05 17:10:00 -0300

    commiteando por eze

--- src/pd/PDMatrix.java
+++ src/pd/PDMatrix.java
@@ -1,21 +1,43 @@
 package pd;
 
+import pd.cells.Cell;
+import pd.utils.Point;
+
 public class PDMatrix {
 
-	public void siblings (int x, int y){
-		
-	}
-
-	public void get (int x, int y){
-		
-	}
-
-	public void remove (int x, int y){
-		
+	private Cell[][] cells;
+	private int rows;
+	private int cols;
+	
+	public PDMatrix (int x, int y){
+		rows=y;
+		cols=x;
+		cells = new Cell[rows][cols];
 	}
 	
-	public void add (int x, int y){
+	public Cell[] siblings (Point p){
+		Cell[] sibs = new Cell[4];
+					
+			cell[i]=
+		}
 		
+		return null;
+	}
+
+	public Cell get (Point p){
+		return  cells[p.getX()][p.getY()];
+	}
+
+	public void remove (Point p){
+		cells[p.getX()][p.getY()]=null;
+	}
+	
+	public boolean add (Point p, Cell cell){
+		if (get(p)!=null){
+			return false;
+		}
+		cells[p.getX()][p.getY()]=cell;
+		return true;
 	}
 
 
