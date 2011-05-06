84ce7ee2a152eed92401bceda7bf77cfc44c4c97
Eze <moroco.ortiz@gmail.com>
2011-05-05 23:52:39 -0300

    Eze salio a arreglar cell

--- src/pd/utils/Cell.java
+++ src/pd/utils/Cell.java
@@ -5,7 +5,7 @@ import pd.utils.Movement;
 public enum Cell {
 	LEFTUP, UPRIGHT, RIGHTDOWN, DOWNLEFT, UPDOWN, LEFTRIGHT, CROSS, WALL, EMPTY, START;
 
-	private Movement direction;
+	private static Movement direction;
 
 	public static Cell[] cells = { Cell.LEFTUP, Cell.UPRIGHT, Cell.RIGHTDOWN,
 			Cell.DOWNLEFT, Cell.UPDOWN, Cell.LEFTRIGHT, Cell.CROSS, Cell.WALL,
@@ -19,7 +19,8 @@ public enum Cell {
 		}
 	}
 
-	public void SetStart(Movement e) {
+	public static void SetStart(Movement e) {
+		
 		direction = e;
 	}
 
