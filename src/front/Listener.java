2fea44503d0986ef2863b40d3ecef7999b9f0647
Max <maximovs@gmail.com>
2011-05-06 18:55:00 -0300

    listener v0.3

--- src/front/Listener.java
+++ src/front/Listener.java
@@ -13,66 +13,70 @@ public class Listener implements EventListener {
 
 	private JFrame frame = new JFrame("Pipe Dream");
 	private BackGround cell;
-
+	private boolean hasStarted=false;
 	@Override
-	public void addCell(Cell c, int x, int y) {
-		// TODO Auto-generated method stub
-
+	public void addCell(Cell c, int i, int j) {
+		if(!hasStarted)
+			return;
+		switch(c){
+		case CROSS:
+			cell.crossCell(i, j);
+			break;
+		case DOWNLEFT:
+			cell.downLeftCell(i, j);
+			break;
+		case EMPTY:
+			cell.emptyCell(i, j);
+			break;
+		case LEFTRIGHT:
+			cell.leftRightCell(i, j);
+			break;
+		case LEFTUP:
+			cell.leftUpCell(i, j);
+			break;
+		case RIGHTDOWN:
+			cell.rightDownCell(i, j);
+			break;
+		case START:
+			cell.startCell(i, j, Cell.startDirection);
+			break;
+		case UPDOWN:
+			cell.upDownCell(i, j);
+			break;
+		case UPRIGHT:
+			cell.upRightCell(i, j);
+			break;
+		case WALL:
+			cell.wallCell(i, j);
+			break;
+		}
 	}
 
 	@Override
 	public void removeCell(int x, int y) {
-		// TODO Auto-generated method stub
-
+		if(!hasStarted)
+			return;
+		
 	}
 
 	@Override
 	public void addAll(Stack s, PDMatrix p) {
-		// TODO Auto-generated method stub
-
+		if(!hasStarted)
+			return;
+		
 	}
 
 	@Override
 	public void initialize(PDMatrix p) {
-		// TODO Auto-generated method stub
+		hasStarted=true;
 		cell = new BackGround(p.getCols(), p.getRows());
 		frame = WindowUtilities.openInJFrame(cell, cell.getCols()
 				* BackGround.size + 15, cell.getRows() * BackGround.size + 39,
 				"Pipe Dream", frame);
 		for(int i=0;i<p.getCols();i++)
 			for(int j=0;j<p.getRows();j++){
-				switch(p.get(new Point(i,j))){
-				case CROSS:
-					cell.crossCell(i, j);
-					break;
-				case DOWNLEFT:
-					cell.downLeftCell(i, j);
-					break;
-				case EMPTY:
-					cell.emptyCell(i, j);
-					break;
-				case LEFTRIGHT:
-					cell.leftRightCell(i, j);
-					break;
-				case LEFTUP:
-					cell.leftUpCell(i, j);
-					break;
-				case RIGHTDOWN:
-					cell.rightDownCell(i, j);
-					break;
-				case START:
-					cell.startCell(i, j, Cell.startDirection);
-					break;
-				case UPDOWN:
-					cell.upDownCell(i, j);
-					break;
-				case UPRIGHT:
-					cell.upRightCell(i, j);
-					break;
-				case WALL:
-					cell.wallCell(i, j);
-					break;
-				}
+				addCell(p.get(new Point(i,j)),i,j);
+				
 			}
 		frame.setContentPane(cell);
 		frame.repaint();
