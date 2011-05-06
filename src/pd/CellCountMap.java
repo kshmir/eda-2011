90b58d030b601db5ced28f753657aa0a756d5deb
Cristian Pereyra <criis.pereyra@gmail.com>
2011-05-05 22:06:22 -0300

    updates

--- src/pd/CellCountMap.java
+++ src/pd/CellCountMap.java
@@ -7,4 +7,8 @@ public abstract class CellCountMap {
 	public abstract int totalPiecesLeft(Class<? extends Cell> t);
 	public abstract int totalPiecesLeft(int index);
 	public abstract int setTotalPiecesLeft(Class<? extends Cell> t, int n);
+	public abstract int decreasePiecesLeft(Class<? extends Cell> t);
+	public abstract int decreasePiecesLeft(int index);
+	public abstract int incrementPiecesLeft(Class<? extends Cell> t);
+	public abstract int incrementPiecesLeft(int index);
 }
