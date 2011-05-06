906249a9516ca1c9845d43f22b9a23ef9e893988
Cristian Pereyra <criis.pereyra@gmail.com>
2011-05-05 22:03:02 -0300

    clase abstracta para eze

--- src/pd/CellCountMap.java
+++ src/pd/CellCountMap.java
@@ -6,5 +6,5 @@ public abstract class CellCountMap {
 	public abstract int totalPiecesLeft();
 	public abstract int totalPiecesLeft(Class<? extends Cell> t);
 	public abstract int totalPiecesLeft(int index);
-	public abstract int setTotalPiecesLeft(Class<? extends Cell> t);
+	public abstract int setTotalPiecesLeft(Class<? extends Cell> t, int n);
 }
