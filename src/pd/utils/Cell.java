09b731678ad610cf709145e7ee11f19f9c6607f2
Eze <moroco.ortiz@gmail.com>
2011-05-05 23:00:33 -0300

    Eze salio a arreglar cell

--- src/pd/utils/Cell.java
+++ src/pd/utils/Cell.java
@@ -3,11 +3,23 @@ package pd.utils;
 import pd.utils.Movement;
 
 public enum Cell {
-	LEFTUP, UPRIGHT, RIGHTDOWN, DOWNLEFT, UPDOWN, LEFTRIGHT, CROSS, WALL, EMPTY, START;
+	LEFTUP(new Movement[] { Movement.LEFT, Movement.UP }), UPRIGHT(
+			new Movement[] { Movement.UP, Movement.RIGHT }), RIGHTDOWN(
+			new Movement[] { Movement.RIGHT, Movement.DOWN }), DOWNLEFT(
+			new Movement[] { Movement.LEFT, Movement.DOWN }), UPDOWN(
+			new Movement[] { Movement.DOWN, Movement.UP }), LEFTRIGHT(
+			new Movement[] { Movement.LEFT, Movement.RIGHT }), CROSS(
+			new Movement[] { Movement.LEFT, Movement.RIGHT, Movement.DOWN,
+					Movement.UP }), WALL(null), EMPTY(null), START(
+			new Movement[] { Movement.NONE });
 
-	private Movement[] moves;
+	private Movement[] direction;
 	private static Cell[] cells = { Cell.LEFTUP, Cell.UPRIGHT, Cell.RIGHTDOWN,
 			Cell.DOWNLEFT, Cell.UPDOWN, Cell.LEFTRIGHT, Cell.CROSS, Cell.WALL,
 			Cell.EMPTY, Cell.START };
+
+	Cell(Movement[] m) {
+		direction = m;
+	}
 
 }
